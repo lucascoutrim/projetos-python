dcbc9abdd5c8d2897572205a43efc5456923e15e
Lucas Coutrim <105084319+lucascoutrim@users.noreply.github.com>
2026-03-04 22:33:24 -0300

    Delete ex016.py

--- ex016.py
+++
@@ -1,9 +0,0 @@
-n1 = int(input('Quantos dias alugado?:'))
-n2 = int(input('Quantos Km Rodados?:'))
-
-di = 60 * n1
-km = n2 * 0.15
-
-res = di + km
-
-print ('O total a pagar é de R${}!'.format(res))
